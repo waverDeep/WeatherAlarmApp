86d59809a3438b952b91f6f99c1665c995d076b0
Jaesun Park <zzangjames75@gmail.com>
2019-09-22 23:15:39 +0900

    Activity for alarm add, weather alarm fragment

---
+++ app/src/main/java/com/example/weatheralarmapp/FragmentWeatherAlarmOpened.java
@@ -0,0 +1,32 @@
+package com.example.weatheralarmapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentWeatherAlarmOpened extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_weather_alarm_closed, container, false);
+
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.imgWeatherAlarmOpen);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlarmAddActivity alarmAddActivity = (AlarmAddActivity) getActivity();
+                alarmAddActivity.onFragmentChange(1);
+            }
+        });
+
+        return rootView;
+    }
+}
