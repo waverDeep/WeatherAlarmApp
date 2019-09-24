2d3336cb7278d0d6fc60673c2788a3ac401c00db
Jaesun Park <zzangjames75@gmail.com>
2019-09-24 19:09:50 +0900

    add id values before merge pull request

--- app/src/main/java/com/example/weatheralarmapp/AlarmAddActivity.java
+++ app/src/main/java/com/example/weatheralarmapp/AlarmAddActivity.java
@@ -2,6 +2,9 @@ package com.example.weatheralarmapp;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,10 +13,34 @@ public class AlarmAddActivity extends AppCompatActivity {
     FragmentWeatherAlarmOpened fragmentWeatherAlarmOpened;
     FragmentWeatherAlarmClosed fragmentWeatherAlarmClosed;
 
+    TextView tvAlarmEditCancel;
+    TextView tvAlarmEditSave;
+
+    ImageView imgRepeatMon;
+    ImageView imgRepeatTue;
+    ImageView imgRepeatWed;
+    ImageView imgRepeatThur;
+    ImageView imgRepeatFri;
+    ImageView imgRepeatSat;
+    ImageView imgRepeatSun;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_add);
+
+        tvAlarmEditCancel = (TextView)findViewById(R.id.tvAlarmEditCancel);
+        tvAlarmEditSave = (TextView)findViewById(R.id.tvAlarmEditSave);
+
+        imgRepeatMon = (ImageView)findViewById(R.id.imgRepeatMon);
+        imgRepeatTue = (ImageView)findViewById(R.id.imgRepeatTue);
+        imgRepeatWed = (ImageView)findViewById(R.id.imgRepeatWed);
+        imgRepeatThur = (ImageView)findViewById(R.id.imgRepeatThur);
+        imgRepeatFri = (ImageView)findViewById(R.id.imgRepeatFri);
+        imgRepeatSat = (ImageView)findViewById(R.id.imgRepeatSat);
+        imgRepeatSun = (ImageView)findViewById(R.id.imgRepeatSun);
+
 
 //        fragmentWeatherAlarmClosed = (FragmentWeatherAlarmClosed)getSupportFragmentManager().findFragmentById(R.id.fragmentWeatherAlarm);
 
@@ -30,6 +57,10 @@ public class AlarmAddActivity extends AppCompatActivity {
 
         fragmentWeatherAlarmOpened = new FragmentWeatherAlarmOpened();
 //        getSupportFragmentManager().beginTransaction().add(R.id.alarmFragmentContainer,fragmentWeatherAlarmOpened).commit();
+
+
+
+
     }
 
     public void onFragmentChange(int index){
